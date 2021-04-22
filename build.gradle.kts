5ac6f4623243d536ea0852f5afe9006b37cd332b
Tlaster <tlaster@outlook.com>
2021-04-22 10:23:27 +0800

    upgrade to compose beta05

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val compose_version by extra("1.0.0-beta04")
+    val compose_version by extra("1.0.0-beta05")
     repositories {
         google()
         mavenCentral()
--- nestedscrollview/build.gradle.kts
+++ nestedscrollview/build.gradle.kts
@@ -40,7 +40,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "moe.tlaster"
                 artifactId = "nestedscrollview"
-                version = "0.1.0"
+                version = "0.2.0"
 
                 from(components["release"])
             }
